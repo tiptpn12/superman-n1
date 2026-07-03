542f3d42d946b19cb4fb66d3e891254380b7055e
robanu-dk <85453277+robanu-dk@users.noreply.github.com>
2026-07-03 17:32:37 +0700

    feat: add fillable

--- app/IsiSppb.php
+++ app/IsiSppb.php
@@ -8,5 +8,5 @@ class IsiSppb extends Model
 {
     protected $table = 'sppb_isi';
     protected $primaryKey = 'sppb_isi_id';
-    protected $fillable = ['sppb_isi_id','sppb_isi_no','sppb_id','master_kode_kbb','master_kode_vendor_id','master_gl_id','master_cost_center_id','master_cash_flow_id'];
+    protected $fillable = ['sppb_isi_id','sppb_isi_no','sppb_id','master_kode_kbb','master_kode_vendor_id','master_gl_id','master_cost_center_id', 'master_profit_center_id','master_cash_flow_id'];
 }
